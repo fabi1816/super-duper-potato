08c4a2df44a3b367a14273e3dddbaccef52d8b44
Cesar Fabian Leguizamon <cesar.leguizamon@globant.com>
2023-04-17 16:08:40 -0300

    Added recipes first views, templates and static content

--- potato-website/recipes/views.py
+++ potato-website/recipes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from recipes.models import Recipe
 
-# Create your views here.
+
+def list_recipes(request):
+    return render(request, "recipes/list.html", context={
+        "recipes_list": Recipe.object.all()
+    })
+
+
+def detailed_recipe(request, pk):
+    recipe = Recipe.objects.get(pk=pk)
+    return render(request, "recipes/details.html", context={
+        "recipe": recipe
+    })
