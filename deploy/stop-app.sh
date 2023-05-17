890c1646bc6a1fc50c3dbd069dec1b6171cb709a
fabi1816 <fabi1816@gmail.com>
2023-05-17 12:08:48 -0300

    Added debug output to stop script

--- deploy/stop-app.sh
+++ deploy/stop-app.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # No need to stop the Nginx server we'll just reload its configuration
+EXISTS=$(ps -ef | pgrep gunicorn)
+
+echo "Does Gunicorn exists? -> $EXISTS"
 
 # Stop Gunicorn if it is running
-if [[ $(ps -ef | pgrep gunicorn) ]]
+if [[ $EXISTS ]]
 then
+    echo "Kill gunicorn"
     killall gunicorn
+else
+    echo "There is NO gunicorn to kill"
 fi
