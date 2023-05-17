20136a2cfcd057deac1bce584dac3617ece786a7
fabi1816 <fabi1816@gmail.com>
2023-05-17 12:50:40 -0300

    Added verify script

---
+++ deploy/verify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo 
+echo "** Is Gunicorn running? **"
+ps -ef | grep gunicorn
+
+echo 
+echo "** How is Nginx? **"
+sudo systemctl statys nginx
+
+echo 
+echo Check the gunicorn logs
+tail /var/log/gunicorn/error.log
+
+echo 
+echo Check the nginx logs
+sudo tail /var/log/nginx/error.log
